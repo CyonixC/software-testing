73aaeab2445778cc82edf6056b1fd65f36707de7
javin_e <javinenghp@gmail.com>
2024-03-21 10:22:07 +0800

    inputs

---
+++ inputs.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <cstddef>
+
+enum class FieldTypes {
+    STRING,
+    INTEGER,
+    BINARY
+};
+
+typedef struct {
+    unsigned int minLen;
+    unsigned int maxLen;
+    FieldTypes type;
+    std::string name;
+    std::vector<std::vector<std::byte>> validChoices;
+} Field;
+
+typedef struct {
+    Field format;
+    std::vector<std::byte> data;
+    unsigned int energy;
+} Input;
+
